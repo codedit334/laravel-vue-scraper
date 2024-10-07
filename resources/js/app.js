9ee061a343f2d7eef1b837849703f0459dd5af07
codedit334 <oussama7elabdioui@gmail.com>
2024-10-07 16:06:38 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import Vue from 'vue';
+import Scraper from './components/Scraper.vue';
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        Scraper,
+    },
+});
