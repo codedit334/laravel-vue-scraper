bf2fff4cf4765acb12c3bb34192433c564f8aa5d
codedit334 <oussama7elabdioui@gmail.com>
2024-10-07 16:01:47 +0100

    Fill controller

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Services\WebScraper;
+
 
 use Illuminate\Http\Request;
 
 class ScraperController extends Controller
 {
-    //
+    protected $scraper;
+
+    public function __construct(WebScraper $scraper)
+    {
+        $this->scraper = $scraper;
+    }
+
+    public function scrape(Request $request)
+    {
+        $url = $request->input('url');
+        $data = $this->scraper->scrape($url);
+
+        return response()->json($data);
+    }
 }
