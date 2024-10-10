3f1dd563a4c5613fd022aedc8cb8f501ceaae59e
codedit334 <oussama7elabdioui@gmail.com>
2024-10-10 11:13:10 +0100

    Create router index

---
+++ resources/js/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// import Home from '../views/Home.vue';
+// import About from '../views/About.vue';
+// import Sportma from '../views/Sportma.vue';
+
+// const routes = [
+//   { path: '/', component: Home },
+//   { path: '/about', component: About },
+//   { path: '/sportma', component: Sportma }
+// ];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
