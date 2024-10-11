02a7ec111b876cfb80ed69d1cbd2da7e32611d8c
codedit334 <oussama7elabdioui@gmail.com>
2024-10-11 12:17:39 +0100

    Create login page

---
+++ resources/js/views/auth/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label>Email</label>
+        <input v-model="email" type="email" required />
+      </div>
+      <div>
+        <label>Password</label>
+        <input v-model="password" type="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        await this.$store.dispatch('login', {
+          email: this.email,
+          password: this.password,
+        });
+        this.$router.push('/');
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
