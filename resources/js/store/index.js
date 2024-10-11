063536e4f2b0633a74cb6673b135ee1fe92a04b9
codedit334 <oussama7elabdioui@gmail.com>
2024-10-11 12:13:42 +0100

    Create store

---
+++ resources/js/store/index.js
@@ -0,0 +1,46 @@
+import { createStore } from 'vuex';
+import axios from 'axios';
+
+export default createStore({
+  state: {
+    user: null,
+    token: localStorage.getItem('token') || '',
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+    setToken(state, token) {
+      state.token = token;
+      localStorage.setItem('token', token);
+    },
+    clearAuthData(state) {
+      state.token = '';
+      state.user = null;
+      localStorage.removeItem('token');
+    },
+  },
+  actions: {
+    async login({ commit }, credentials) {
+      const response = await axios.post('/api/login', credentials);
+      commit('setToken', response.data.token);
+      commit('setUser', response.data.user);
+    },
+    async logout({ commit }) {
+      await axios.post('/api/logout');
+      commit('clearAuthData');
+    },
+    async getUser({ commit }) {
+      const response = await axios.get('/api/user');
+      commit('setUser', response.data);
+    },
+  },
+  getters: {
+    isAuthenticated(state) {
+      return !!state.token;
+    },
+    getUser(state) {
+      return state.user;
+    },
+  },
+});
