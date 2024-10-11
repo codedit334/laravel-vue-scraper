3e3e1c56e1daff2718191472f07449fe2ccfe202
codedit334 <oussama7elabdioui@gmail.com>
2024-10-11 12:18:49 +0100

    Create login route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../pages/App.vue';
+import Login from '../views/auth/Login.vue';
 import Sportma from '../pages/Sportma.vue';
 import Register from '../views/auth/Register.vue';
 import Scraper from '../components/Scraper.vue';
 
 const routes = [
   { path: '/', component: Sportma },
+  { path: '/login', component: Login },
   { path: '/register', name: 'register', component: Register },
   { path: '/sportma', component: Sportma },
   { path: '/scrape', component: Scraper }
