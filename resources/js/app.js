75df7cc0c5437121cee81366e79e549210112bf2
codedit334 <oussama7elabdioui@gmail.com>
2024-10-16 10:30:18 +0100

    Register swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,10 @@ import router from './router';
 import axios from 'axios';
 import store from './store';
 
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
